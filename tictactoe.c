4d103091e92a529e3867f9b946a576e3d78f526d
Sebastian Cursaro <scursaro@cpp.edu>
2022-10-07 15:23:23 -0700

    Comments, and ideas for the code

---
+++ tictactoe.c
@@ -0,0 +1,25 @@
+//Game between two players
+
+int main(){
+    //prompt user for game they wish t play
+
+
+    //read information from console
+
+    //possibly start the game, maybe a while loop with a nested switch
+    //Will also need two separate cases, One for With Player, and One with random 
+
+    //after selection is made, prompt user to enter two int's for their location
+    //check if the location is taken, if not place guess, else return to user to pick new spot
+
+    //print out the new layout
+
+    //maybe also have an array? or keep track somehow 
+    
+    
+    //have a way to keep track of three in a row possibly a function that is for add(), and 
+    // another function to check if the person on didWin()
+    
+    
+    return 0;
+}
